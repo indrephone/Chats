a332ba76237e036d8e3e4724a97d16ece3d52308
indrephone <indrephone@gmail.com>
2024-10-28 12:04:39 +0200

    header icons

--- client/src/components/UI/organisms/Header.tsx
+++ client/src/components/UI/organisms/Header.tsx
@@ -1,8 +1,34 @@
+import styled from 'styled-components';
+import { NavLink, Link, useNavigate } from 'react-router-dom';
+
 const Header = () => {
     return ( 
-        <section>
+        <header>
+            <nav>
+                <ul>
+                   <li><NavLink to="/conversations">
+                      <img src="/chat_white.svg" alt="chat button" /></NavLink></li>
+
+
+                   <li><NavLink to="/users">
+                     <img src="star_white.svg" alt="users button" /></NavLink></li>
+                </ul>
+            </nav>
+
+            <div>
+                <img src="https://upload.wikimedia.org/wikipedia/en/b/b2/Pluto_%28Disney%29_transparent.png"
+                alt="user profile picture"
+                 />
+                <span>Username</span> 
+                <button
+                  onClick={() => {
+                    // setLoggedInUser('');
+                    // navigate('')
+                  }}
+                  >Log Out</button>
+            </div>
             
-        </section>
+        </header>
      );
 }
  
