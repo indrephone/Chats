c4df00c8528da9eff677dc2fd08a4431d15baaf3
indrephone <indrephone@gmail.com>
2024-10-30 16:16:47 +0200

    AllUsersCard style adjusted and links added to navigate to userPage

--- client/src/components/UI/molecules/AllUsersCard.tsx
+++ client/src/components/UI/molecules/AllUsersCard.tsx
@@ -6,21 +6,45 @@ import {  UserType } from '../../../contexts/UsersContext';
 type Props ={
     data: UserType;
 }
-
+const CardLink = styled(Link)`
+  text-decoration: none;
+  color: inherit;
+`;
 
 const CardDiv = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 10px;
+  padding: 20px;
+  background-color: #f5f5f5;
+  border-bottom: 1px solid #ccc;
+  cursor: pointer;
   
+  img {
+    width: 40px;
+    height: 40px;
+    border-radius: 50%;
+  }
+
+  span {
+    font-size: 16px;
+    font-weight: normal;
+    margin: 0;
+  }
 `;
 
 const AllUsersCard = ({data} : Props) => {
     return ( 
-        <CardDiv>
+
+        <CardLink to={`/userPage/${data._id}`}> 
+           <CardDiv>
             <img 
-              src={data.profileImage || "/default_profile_image.svg"}
-              alt={data.username}
-              />
-              <h3>{data.username}</h3>
-        </CardDiv>
+                src={data.profileImage || "/default_profile_image.svg"}
+                alt={data.username}
+                />
+            <span>{data.username}</span>
+           </CardDiv>
+        </CardLink>
      );
 }
  
