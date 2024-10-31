8ab0e79d3b3cef92e3a63a0785d41616194287b2
indrephone <indrephone@gmail.com>
2024-10-31 10:15:05 +0200

    profile.tsx basic info

--- client/src/components/pages/Profile.tsx
+++ client/src/components/pages/Profile.tsx
@@ -1,7 +1,17 @@
+import { useContext} from 'react';
+import UsersContext,{ UsersContextTypes } from '../../contexts/UsersContext';
+
+
 const Profile = () => {
+   const { loggedInUser } = useContext(UsersContext) as UsersContextTypes;
+
+   if(!loggedInUser) return <p>Loading...</p>;
+
     return ( 
         <section>
             <h1>Profile - Welcome to the Chat Palace!</h1>
+            <p>{loggedInUser.username}</p>
+            <img src={loggedInUser.profileImage} alt={`${loggedInUser.username}'s profile`} />
         </section>
      );
 }
